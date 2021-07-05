afdacdeb9a206e74a422b464afe7039ada8d55c6
Daria Svarin <d.svarin.98@gmail.com>
2021-07-05 11:35:35 +0300

    Start

---
+++ src/components/moviapi-serves.js
@@ -0,0 +1,12 @@
+// export default class MoviesapiService {
+
+//     async getResource() {
+//         const response = await fetch(url);
+
+//         if (!response.ok) {
+//             throw mew Error(`Couldn't fetch ${url}, received ${res.status}`)
+//         }
+//         return await response.json();
+//     }
+
+// }
